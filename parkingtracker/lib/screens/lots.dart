a619cdcaab66697755f9ab0f5cc0375e4c8a62e1
8blocker <ps3evanps3@gmail.com>
2025-09-03 13:17:05 -0400

    Parking lot list view added.

--- parkingtracker/lib/screens/lots.dart
+++ parkingtracker/lib/screens/lots.dart
@@ -13,13 +13,27 @@ class LotsScreen extends StatefulWidget {
 class _LotsScreenState extends State<LotsScreen> {
   // List of different lots to choose from go here. Info like Name & how many spots available needed.
   // Stateful logic here (things that change)
+
+  // This will need to be changed to pull from backend or internal storage
+  List<String> lotList = ['Engineering Lot 1', 'Engineering Lot 2', 'Rocket Stadium Lot 15'];
   
   // Build methods called anytime Flutter rebuilds UI, returns Widget
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 227, 210, 248),
-      body: Center(child: Text('Parking Lot List here')),
+      body: ListView.separated( // Create list of lots to choose from using itemBuilder (which allows us to dynamically create it)
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            height: 60,
+            color: Colors.lightBlue,
+            child: Center(child: Text(lotList[index]))
+            );
+        },
+        padding: const EdgeInsets.all(15), 
+        separatorBuilder: (BuildContext context, int index) => const Divider(), 
+        itemCount: lotList.length,
+      ),
     );
   }
 }
