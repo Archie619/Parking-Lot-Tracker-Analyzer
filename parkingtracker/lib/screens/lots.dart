87241dca75cd5e598b3235aef62a4a79c43fd071
8blocker <ps3evanps3@gmail.com>
2025-09-07 18:19:48 -0400

    Parking lot info screen added showing simplified map & # of spots available. Added on tap gesture to select specific lot from list.

--- parkingtracker/lib/screens/lots.dart
+++ parkingtracker/lib/screens/lots.dart
@@ -1,6 +1,9 @@
 // Lots widget that will hold list of lots to choose from.
 
 import 'package:flutter/material.dart';
+import 'package:parkingtracker/lot.dart';
+import 'package:parkingtracker/screens/lotInfo.dart';
+import 'package:parkingtracker/screens/screen.dart';
 
 // Stateful = Mutable
 class LotsScreen extends StatefulWidget {
@@ -15,24 +18,59 @@ class _LotsScreenState extends State<LotsScreen> {
   // Stateful logic here (things that change)
 
   // This will need to be changed to pull from backend or internal storage
-  List<String> lotList = ['Engineering Lot 1', 'Engineering Lot 2', 'Rocket Stadium Lot 15'];
+
+  List<Lot> lots = [
+    Lot('Engineering Lot 1', 1, 70, 100, 'https://i.ibb.co/7dKrFp0T/simplified-Lot.png'),
+    Lot('Engineering Lot 2', 2, 30, 70, 'https://i.ibb.co/7dKrFp0T/simplified-Lot.png'),
+    Lot('Rocket Stadium Lot 15', 3, 100, 150, 'https://i.ibb.co/7dKrFp0T/simplified-Lot.png'),
+  ];
   
   // Build methods called anytime Flutter rebuilds UI, returns Widget
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 227, 210, 248),
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 86, 163, 227),
+        foregroundColor: Colors.white,
+        title: Text('Available Parking Lots'),
+      ),
       body: ListView.separated( // Create list of lots to choose from using itemBuilder (which allows us to dynamically create it)
         itemBuilder: (BuildContext context, int index) {
-          return Container(
+          return InkWell( // Allows us to recognize taps on specific containers (lots)
+            onTap: () {
+              Navigator.push( // Push new lotsInfoScreen to front and give current info on it.
+                context,
+                MaterialPageRoute<void>(
+                  builder: (context) => LotsInfoScreen(lot: lots[index]),
+                  ),
+              );
+            },
+            child: Container(
+            padding: EdgeInsets.only(left: 15.0, right: 25.0), // pad both left/right of container
             height: 60,
-            color: Colors.lightBlue,
-            child: Center(child: Text(lotList[index]))
-            );
+            decoration: BoxDecoration(
+              color: Colors.greenAccent,
+              border: Border.all(
+                color: Colors.black, 
+                width: 4,
+              )
+            ),
+            child: 
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween, // put max space between children
+                children: [
+                  Text(lots[index].lotName), // show lot name
+                  Text('${lots[index].availableSpots} spots left!'), // show # of spots left
+                ],
+              ),
+            ),
+          );
+            
         },
         padding: const EdgeInsets.all(15), 
         separatorBuilder: (BuildContext context, int index) => const Divider(), 
-        itemCount: lotList.length,
+        itemCount: lots.length,
       ),
     );
   }
