90d294eccdd0e2cd3320e42cc34471d18619bc9c
Nate <ezspams56@gmail.com>
2025-09-10 13:11:34 -0400

    add detection zone analysis

--- backend/analysis/opencv_sandbox.py
+++ backend/analysis/opencv_sandbox.py
@@ -61,7 +61,54 @@ for spot in spots:
     cv2.line(detection_zones_img, p3, p4, (0, 0, 255), 3)
     cv2.line(detection_zones_img, p4, p1, (0, 0, 255), 3)
 
+# with spots marked we need to check if a car is in
+# the spot or not; use background subtraction
+# NOTE: WE NEED TO KEEP A SAMPLE OF THE EMPTY LOT FOR ME TO DO THIS
+#       i.e during initialization take a snap of the lot and store 
+#       in DB
+empty_lot = cv2.imread('..\images\diag_PL2.jpg', 1)
+empty_lot = cv2.resize(empty_lot, (img_w, img_h))
+live_lot = cv2.imread('..\images\live_diag_PL2.jpg', 1)
+live_lot = cv2.resize(live_lot, (img_w, img_h))
+
+# subtract the empty lot from the live lot, gray scale for color
+# consistency
+subtracted_lot = cv2.absdiff(empty_lot, live_lot)
+gray_subtracted = cv2.cvtColor(subtracted_lot, cv2.COLOR_BGR2GRAY)
+
+# check spot detection zones for certain amount of changed
+# pixels; if over threshold, spot occupied
+spot_occupancy = []
+for spot in spots:
+    mask = numpy.zeros((img_h, img_w), dtype=numpy.uint8)
+    cv2.fillPoly(mask, [numpy.array(spot)], (255, 255, 255))
+    isolated_spot = cv2.bitwise_and(gray_subtracted, gray_subtracted, mask=mask)
+
+    spot_area = cv2.countNonZero(mask)
+    changed_pixels = cv2.countNonZero(isolated_spot)
+    percent_filled = (changed_pixels / spot_area) * 100
+
+    if percent_filled > 20:
+        spot_occupancy.append(1)
+    else:
+        spot_occupancy.append(0)
+
+# after detection zone statuses are determined overlay the color
+# on the live image
+i = 0
+overlay = live_lot.copy()
+for spot in spots:
+    if spot_occupancy[i] == 1:
+        cv2.fillPoly(overlay, [numpy.array(spot)], (0, 255, 0))
+    else:
+        cv2.fillPoly(overlay, [numpy.array(spot)], (0, 0, 255))
+    i += 1
+
+# apply a transparency filter over the original image
+transparency = 0.3
+cv2.addWeighted(overlay, transparency, live_lot, 1 - transparency, 0, live_lot) 
+
 # display the images
-cv2.imshow('Current Analysis', detection_zones_img)
+cv2.imshow('Analysis', live_lot)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
