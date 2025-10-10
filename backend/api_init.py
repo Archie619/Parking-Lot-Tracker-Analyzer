3f5596729adadf75d42043df16ff1ec7faa40838
Nate <ezspams56@gmail.com>
2025-10-10 12:49:03 -0400

    add spot map db capability

--- backend/api_init.py
+++ backend/api_init.py
@@ -26,11 +26,37 @@ for lot in saved_lots:
 
     numpy_byte_img = numpy.frombuffer(lot[1], dtype=numpy.uint8)
     empty_lot_img = cv2.imdecode(numpy_byte_img, cv2.IMREAD_COLOR)
+
+    # retrieve spot map
+    cursor.execute('SELECT lr.row_index, s.space_number, spp.point_index, spp.x, spp.y '
+                   'FROM lot_row AS lr '
+                    'JOIN space AS s '
+                        'ON lr.row_id = s.row_id '
+                    'JOIN space_polygon_point AS spp '
+                        'ON s.space_id = spp.space_id '
+                    'WHERE lr.lot_code = ?', (lot[0],))
+    spot_points = cursor.fetchall()
     
+    # build spot map
+    spots = [[[]]]
+    row = 0
+    spot = 0
+    curr_space_id = 0
+    for point in spot_points:
+        if point[0] != row:
+            spot = -1
+            row += 1
+            spots.append([])
+        if point[1] != curr_space_id:
+            curr_space_id = point[1]
+            spot += 1
+            spots[row].append([])
+        spots[row][spot].append((point[3], point[4]))
+
     lots.append({'name': lot[0],
                  'empty_lot_img': empty_lot_img,
                  'live_lot_stream': lot[2] + '?rtsp_transport=tcp&stimeout=2000000',
-                 'spots': define_spots(cv2.resize(empty_lot_img, (img_w, img_h)))})
+                 'spots': spots})
 
 threading.Thread(target=begin_rolling_analysis, daemon=True).start()
 
--- backend/routers/lot_init.py
+++ backend/routers/lot_init.py
@@ -55,16 +55,51 @@ async def init_lot(lot: NewLot):
     # add the lot to the rolling analysis and database, if everything has been
     # successful up to this point
     if status == 'success':
+
+        spot_map = define_spots(cv2.resize(ss, (img_w, img_h)))
+
+        # add lot to rolling analysis
         lots.append({'name': lot.lot_name,
                      'empty_lot_img': ss,
                      'live_lot_stream': lot.lot_feed_source + '?rtsp_transport=tcp&stimeout=2000000',
-                     'spots': define_spots(cv2.resize(ss, (img_w, img_h)))})
+                     'spots': spot_map})
         
         # encode image to bytes
         success, img_bytes = cv2.imencode(".png", ss)
+        
+        # add overall lot information
         cursor.execute('INSERT INTO lot_media (lot_code, empty_img_path, live_stream)' 
                        'VALUES (?, ?, ?)', (lot.lot_name, img_bytes.tobytes(), lot.lot_feed_source))
         db_con.commit()
+
+        last_space = 0
+        # add lot row information
+        for i in range(0, len(spot_map)):
+            cursor.execute('INSERT INTO lot_row (lot_code, row_index)'
+                           'VALUES (?, ?)', (lot.lot_name, i))
+            db_con.commit()
+            cursor.execute('SELECT row_id FROM lot_row WHERE lot_code = ? AND row_index = ?', 
+                           (lot.lot_name, i))
+            row_id = cursor.fetchone()[0]
+
+            # add spot information
+            for j in range(0, len(spot_map[i])):
+                cursor.execute('INSERT INTO space (lot_code, space_number, row_id)'
+                               'VALUES (?, ?, ?)', (lot.lot_name, last_space + j, row_id))
+                db_con.commit()
+                cursor.execute('SELECT space_id FROM space '
+                               'WHERE lot_code = ? AND space_number = ? AND row_id = ?', 
+                               (lot.lot_name, last_space + j, row_id))
+                space_id = cursor.fetchone()[0]
+
+                # add spot mapping information
+                for k in range(0, 4):
+                    cursor.execute('INSERT INTO space_polygon_point (space_id, point_index, '
+                                   'x, y) VALUES (?, ?, ?, ?)', 
+                                   (space_id, k, float(spot_map[i][j][k][0]), 
+                                    float(spot_map[i][j][k][1])))
+                    db_con.commit()
+            last_space += len(spot_map[i])
 
     return {'status': status,
             'message': message}
